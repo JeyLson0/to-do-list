3f3aed23017d4b9cb36eabc9d7b47cab0699b3d1
Jeylson0 <jhonalson.lugada@gmail.com>
2024-01-27 03:06:48 +0800

    Version 1.0.2 CSS Body grid layout
    Changed CSS layout from flexbox to grid to create a responsive layout.
    Once min-width reached 600px nav bar moves to the bottom from the left
    side of the webpage.

--- src/assets/scripts/imgIndex.js
+++ src/assets/scripts/imgIndex.js
@@ -4,15 +4,18 @@ export const priorityIcon = require('../imgs/priority-icon.svg');
 export const hashtagIcon = require('../imgs/project-hashtags.svg');
 export const projectIcon = require('../imgs/project-icon.svg');
 export const todayIcon = require('../imgs/today-icon.svg');
+export const addIcon = require('../imgs/add-icon.svg')
 
 const DOMCompleteIcon = document.querySelector('img[alt="complete-icon"]');
 const DOMMenuIcon = document.querySelector('img[alt="menu-icon"]');
 const DOMPriorityIcon = document.querySelector('img[alt="priority-icon"]');
 const DOMProjectIcon = document.querySelector('img[alt="project-icon"]');
 const DOMTodayIcon = document.querySelector('img[alt="today-icon"]');
+const DOMAddItem = document.querySelector('#add-item')
 
 DOMCompleteIcon.setAttribute('src', completeIcon);
 DOMMenuIcon.setAttribute('src', menuIcon);
 DOMPriorityIcon.setAttribute('src', priorityIcon);
 DOMProjectIcon.setAttribute('src', projectIcon);
 DOMTodayIcon.setAttribute('src', todayIcon);
+DOMAddItem.setAttribute('src', addIcon)
